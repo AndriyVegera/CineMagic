f9828a45c1ec1eca3ce65016a8a611b6030e006a
AndriyVegera <vegeraandrij@gmail.com>
2023-07-04 15:23:47 +0300

    add TMDB API

--- cm/src/App.js
+++ cm/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {fetchDataFromApi} from "./utils/api";
+import {useEffect} from "react";
 
 function App() {
+    useEffect(()=>{
+        apiTesting();
+    },[])
+    const apiTesting = ()=>{
+        fetchDataFromApi('/movie/popular?api_key=dbf166fac1aa4fe7d19eb9a2ab837477')
+            .then((res)=>console.log(res))
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        App
     </div>
   );
 }
