20aa76541b3ecb786e939fc9a20a6dcc60f40a08
AndriyVegera <vegeraandrij@gmail.com>
2023-07-04 16:37:01 +0300

    add routes

--- cm/src/App.js
+++ cm/src/App.js
@@ -3,6 +3,15 @@ import {fetchDataFromApi} from "./utils/api";
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getApiConfiguration} from "./store/homeSlice";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+function SearchResult() {
+    return null;
+}
 
 function App() {
     const dispatch = useDispatch();
@@ -18,10 +27,15 @@ function App() {
             })
     }
   return (
-    <div className="App">
-        App
-        {/*{url?.total_pages}*/}
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/:mediaType/:id" element={<Details />} />
+              <Route path="/search/:query" element={<SearchResult />} />
+              <Route path="/explore/:mediaType" element={<Explore />} />
+              <Route path="*" element={<PageNotFound />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
